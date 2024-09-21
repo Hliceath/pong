26db7f6189c11cecee68b4be415174d37da107c5
Hliceath <hliceath@protonmail.com>
2024-09-21 20:35:49 +0200

    add ball scene

--- ball.gd
+++ ball.gd
@@ -1,7 +1,6 @@
-extends Area2D
-signal hit
+extends RigidBody2D
 
-@export var speed: int = 550
+@export var speed: int = 400
 var screen_size: Vector2
 
 func _ready() -> void:
@@ -11,21 +10,13 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	var velocity: Vector2 = Vector2.ZERO
 
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
+	velocity.x += 1
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
 
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
-
-func _on_body_entered(body: PhysicsBody2D) -> void:
-	hide()
-	hit.emit()
-	$CollisionShape2D.set_deferred("disabled", true)
 
 func start(pos: Vector2) -> void:
 	position = pos
